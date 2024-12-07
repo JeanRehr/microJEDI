2d2dcf58f3e62a290dffee9348668bf2db0b8fd7
Jean Rehr <49696706+JeanRehr@users.noreply.github.com>
2024-12-07 16:27:04 -0300

    Update ALU.java

--- com/objmicrojedi/ALU.java
+++ com/objmicrojedi/ALU.java
@@ -1,31 +1,31 @@
 package com.objmicrojedi;
 
 public class ALU {
-    public void sum (Registers registers) {
+    public void sum(Registers registers) {
         Register addedValue = new Register(registers.A.getValue());
         addedValue.add(registers.B.getValue());
         registers.C.setValue(addedValue.getValue());
     }
     
-    public void sub (Registers registers) {
+    public void sub(Registers registers) {
         Register subValue = new Register(registers.A.getValue());
         subValue.subtract(registers.B.getValue());
         registers.C.setValue(subValue.getValue());
     }
 
-    public void mul (Registers registers) {
+    public void mul(Registers registers) {
         Register mulValue = new Register(registers.A.getValue());
         mulValue.multiply(registers.B.getValue());
         registers.C.setValue(mulValue.getValue());
     }
 
-    public void div (Registers registers) throws ArithmeticException {
+    public void div(Registers registers) throws ArithmeticException {
         Register divValue = new Register(registers.A.getValue());
         divValue.divide(registers.B.getValue());
         registers.C.setValue(divValue.getValue());
     }
 
-    public void com (Registers registers) {
+    public void com(Registers registers) {
         if (registers.A.getValue() == registers.B.getValue()) {
             registers.C.setValue(0);
         } else if (registers.A.getValue() < registers.B.getValue()) {
